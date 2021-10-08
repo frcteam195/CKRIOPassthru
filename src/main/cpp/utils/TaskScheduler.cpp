d194fe5ccc11c2d8d860ae40509428ea566cdb61
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-10-08 17:05:34 -0400

    Remove some debug code

--- src/main/cpp/utils/TaskScheduler.cpp
+++ src/main/cpp/utils/TaskScheduler.cpp
@@ -49,12 +49,9 @@ void TaskScheduler::stop()
 void TaskScheduler::run()
 {
     ck::configureThreadPriority(90);
-    // int xRun = 0;
     while (threadActive)
     {
         timeNow = HAL_GetFPGATime(&c_status);
-        //Timing based on previous work from here: https://github.com/guitar24t/SPARK-MAX-roboRIO/blob/master/src/main/driver/cpp/CANSparkMaxSetDriver.cpp
-        //TODO: Consider converting this to use HAL_Notifier and do own thread management instead of using Notifier
         for (Task *t : taskList)
         {
             if (t)
@@ -72,22 +69,11 @@ void TaskScheduler::run()
                     t->timeNextUpdateuS = t->timeLastUpdateuS + (t->taskRateMs * 1000);
                     nextWakeTime = nextWakeTime < timeNow ? t->timeNextUpdateuS : nextWakeTime;
                     nextWakeTime = ck::math::min(t->timeNextUpdateuS, nextWakeTime);
-
-                    // if (xRun++ % 50 == 0) {
-                    //     std::cout << "Time now: " << timeNow << std::endl;
-                    //     std::cout << "Next update: " << t->timeNextUpdateuS << std::endl;
-                    //     std::cout << "Last update: " << t->timeLastUpdateuS << std::endl;
-                    //     std::cout << "Next wake time: " << nextWakeTime << std::endl;
-                    // }
                 }
             }
         }
-        //TODO: Test this timing, this doesn't likely work yet.
         HAL_UpdateNotifierAlarm(m_notifier, nextWakeTime, &c_status);
         uint64_t sleepTime = HAL_WaitForNotifierAlarm(m_notifier, &c_status) - timeNow;
-        // if (xRun % 50 == 0) {
-        //     std::cout << "Actual sleep time: " << sleepTime << std::endl;
-        // }
         sleepTime = 0;
     }
 }
