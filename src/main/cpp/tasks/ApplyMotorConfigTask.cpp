a07f70b0fce2f66fb8abd2757eb196f6a98003a5
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-10-10 02:01:02 -0400

    Add phoenix helper for function retry

--- src/main/cpp/tasks/ApplyMotorConfigTask.cpp
+++ src/main/cpp/tasks/ApplyMotorConfigTask.cpp
@@ -7,6 +7,7 @@
 #include "MotorManager.hpp"
 #include "ctre/Phoenix.h"
 #include "NetworkManager.hpp"
+#include "utils/PhoenixHelper.hpp"
 
 ApplyMotorConfigTask::ApplyMotorConfigTask() : Task(THREAD_RATE_MS) {}
 
@@ -23,11 +24,15 @@ void ApplyMotorConfigTask::run(uint32_t timeSinceLastUpdateMs)
     {
         for (ck::MotorConfiguration_Motor const& m : motorsUpdate.motors())
         {
+            //TODO: Implement per command differential set only if value is changed
             ck::MotorConfiguration_Motor_MotorControllerConfiguration config = m.motor_configuration();
             MotorManager::getInstance().forEach([&] (uint16_t id, BaseMotorController* mCtrl, MotorType mType)
             {
-                int kTimeoutMs = 10;
-                mCtrl->Config_kP(0, config.kp(), kTimeoutMs);
+                ck::runTalonFunctionWithRetry([&]() { return mCtrl->Config_kP(0, config.kp(), ck::kCANTimeoutMs); }, id);
+                ck::runTalonFunctionWithRetry([&]() { return mCtrl->Config_kI(0, config.ki(), ck::kCANTimeoutMs); }, id);
+                ck::runTalonFunctionWithRetry([&]() { return mCtrl->Config_kD(0, config.kd(), ck::kCANTimeoutMs); }, id);
+                ck::runTalonFunctionWithRetry([&]() { return mCtrl->Config_kF(0, config.kf(), ck::kCANTimeoutMs); }, id);
+                
             });
         }
     }
