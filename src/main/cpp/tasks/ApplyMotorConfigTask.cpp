15e2ae7b2ac1748f6af21cf3b674b2ba16351f6b
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-10-12 19:44:32 -0400

    Update motor config

--- src/main/cpp/tasks/ApplyMotorConfigTask.cpp
+++ src/main/cpp/tasks/ApplyMotorConfigTask.cpp
@@ -38,6 +38,52 @@ void ApplyMotorConfigTask::run(uint32_t timeSinceLastUpdateMs)
                     ck::runTalonFunctionWithRetry([&]() { return mCtrl->Config_kI(0, config.ki(), ck::kCANTimeoutMs); }, id);
                     ck::runTalonFunctionWithRetry([&]() { return mCtrl->Config_kD(0, config.kd(), ck::kCANTimeoutMs); }, id);
                     ck::runTalonFunctionWithRetry([&]() { return mCtrl->Config_kF(0, config.kf(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->Config_IntegralZone(0, config.izone(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigMaxIntegralAccumulator(0, config.max_i_accum(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigAllowableClosedloopError(0, config.allowed_closed_loop_error(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigClosedLoopPeakOutput(0, config.max_closed_loop_peak_output(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigMotionCruiseVelocity(config.motion_cruise_velocity(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigMotionAcceleration(config.motion_acceleration(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigMotionSCurveStrength(config.motion_s_curve_strength(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigForwardSoftLimitThreshold(config.forward_soft_limit(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigForwardSoftLimitEnable(config.forward_soft_limit_enable(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigReverseSoftLimitThreshold(config.reverse_soft_limit(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigReverseSoftLimitEnable(config.reverse_soft_limit_enable(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigSelectedFeedbackCoefficient(config.feedback_sensor_coefficient(), 0, ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigVoltageCompSaturation(config.voltage_compensation_saturation(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { mCtrl->EnableVoltageCompensation(config.voltage_compensation_enabled()); return mCtrl->GetLastError(); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { mCtrl->SetInverted((InvertType)config.invert_type()); return mCtrl->GetLastError(); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { mCtrl->SetSensorPhase(config.sensor_phase_inverted()); return mCtrl->GetLastError(); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { mCtrl->SetNeutralMode((NeutralMode)config.neutral_mode()); return mCtrl->GetLastError(); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigOpenloopRamp(config.open_loop_ramp(), ck::kCANTimeoutMs); }, id);
+                    ck::runTalonFunctionWithRetry([&]() { return mCtrl->ConfigClosedloopRamp(config.closed_loop_ramp(), ck::kCANTimeoutMs); }, id);
+                    
+                    const ck::MotorConfiguration_Motor_MotorControllerConfiguration_CurrentLimitConfiguration* supplyCurrLimConfig = &config.supply_current_limit_config();
+                    
+                    switch (mType)
+                    {
+                    case MotorType::TALON_FX:
+                    {
+                        const ck::MotorConfiguration_Motor_MotorControllerConfiguration_CurrentLimitConfiguration* statorCurrLimConfig = &config.stator_current_limit_config();
+                        TalonFX* tfx = dynamic_cast<TalonFX*>(mCtrl);
+                        ck::runTalonFunctionWithRetry([&]() { return tfx->ConfigSupplyCurrentLimit(SupplyCurrentLimitConfiguration(supplyCurrLimConfig->enable(), supplyCurrLimConfig->current_limit(), supplyCurrLimConfig->trigger_threshold_current(), supplyCurrLimConfig->trigger_threshold_time()), ck::kCANTimeoutMs); }, id);
+                        ck::runTalonFunctionWithRetry([&]() { return tfx->ConfigStatorCurrentLimit(StatorCurrentLimitConfiguration(statorCurrLimConfig->enable(), statorCurrLimConfig->current_limit(), statorCurrLimConfig->trigger_threshold_current(), statorCurrLimConfig->trigger_threshold_time()), ck::kCANTimeoutMs); }, id);
+                    }
+                        break;
+                    case MotorType::TALON_SRX:
+                    {
+                        TalonSRX* tsrx = dynamic_cast<TalonSRX*>(mCtrl);
+                        ck::runTalonFunctionWithRetry([&]() { return tsrx->ConfigSupplyCurrentLimit(SupplyCurrentLimitConfiguration(supplyCurrLimConfig->enable(), supplyCurrLimConfig->current_limit(), supplyCurrLimConfig->trigger_threshold_current(), supplyCurrLimConfig->trigger_threshold_time()), ck::kCANTimeoutMs); }, id);
+                    }
+                        break;
+                    default:
+                        break;
+                    }
+
+                    if (config.fast_master())
+                    {
+
+                    }
                     
                 });
             }
