56a9cca1a0b17e99c4627da0928e5015ed42241c
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-10-29 14:00:36 -0400

    Begin rate controlled logger framework

--- src/main/include/utils/CKLogger.hpp
+++ src/main/include/utils/CKLogger.hpp
@@ -5,24 +5,52 @@
 #include <ostream>
 #include <sstream>
 #include <unordered_map>
+#include <iostream>
 
 
 class CKLogger
 {
 public:
-    template<typename T>
-    const CKLogger& operator<<(const T& v) const;
-    const CKLogger& operator<<(std::ostream& (*F)(std::ostream&)) const;
-    void messagePump();
+    template<class T>
+    const CKLogger& operator<<(const T& v)
+    {
+        std::scoped_lock<std::mutex> lock(listLock);
+        std::string s = toString(v);
+        if (!msgList.count(s))
+        {
+            msgList[s] = 0;
+            mStream << v << std::endl;
+        }
+        return *this;
+    }
 
-    CKLogger();
-    ~CKLogger();
+    const CKLogger& operator<<(std::ostream& (*F)(std::ostream&)) const
+    {
+        // std::scoped_lock<std::mutex> lock(listLock);
+        return *this;
+    }
+
+    void messagePump()
+    {
+        std::scoped_lock<std::mutex> lock(listLock);
+        std::cout << mStream.rdbuf();
+        msgList.clear();
+    }
+
+    CKLogger() {}
+    ~CKLogger() {}
 private:
     std::mutex listLock;
     std::unordered_map<std::string, int> msgList;
+    std::stringstream mStream;
 
     template<typename T>
-    static std::string toString(const T& x);
+    std::string toString(const T& x)
+    {
+        std::ostringstream os;
+        os << x;
+        return os.str();
+    }
 };
 
 extern CKLogger ckLogger;
