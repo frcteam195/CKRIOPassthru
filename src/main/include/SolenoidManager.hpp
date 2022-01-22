54c66db9485b3396c3027a003ef863256b144bbf
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-01-22 02:19:13 -0500

    Begin work on pneumatics support

---
+++ src/main/include/SolenoidManager.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "DataStructures.hpp"
+#include "utils/Singleton.hpp"
+#include "ctre/Phoenix.h"
+#include <vector>
+#include <map>
+#include <mutex>
+#include <functional>
+#include "IntellisenseDefs.h"
+#include "MotorConfiguration.pb.h"
+
+class SolenoidManager : public Singleton<SolenoidManager>
+{
+    friend Singleton;
+public:
+    void registerSolenoid(uint16_t id, MotorType motorType);
+    void deleteMotor(uint16_t id);
+    void onMotor(uint16_t id, std::function<void(uint16_t, BaseMotorController*, MotorType)> func);
+    void onMotor(const google::protobuf::Message& msg, std::function<void(uint16_t, BaseMotorController*, MotorType, const ck::MotorConfiguration::Motor&)> func);
+    void forEach(std::function<void(uint16_t, BaseMotorController*, MotorType)> func);
+    void processHeartbeat();
+    //getMotor_threadsafe will lock the data structures before accessing, however, it can still return nullptr if motor does not exist
+    BaseMotorController* getMotor_threadsafe(uint16_t id);
+    //UNSAFE Methods will not lock the data structures before accessing. Be careful with usage
+    BaseMotorController* getMotor_unsafe(uint16_t id);
+private:
+    SolenoidManager();
+    ~SolenoidManager();
+    std::map<uint16_t, BaseMotorController*> mRegisteredMotorList;
+    std::map<uint16_t, MotorType> mRegisteredMotorTypeList;
+    std::map<uint16_t, int> mRegisteredMotorHeartbeatList;
+
+    std::mutex motorLock;
+
+    void deleteMotor_internal_unsafe(uint16_t id);
+
+    static constexpr int kMaxHeartbeatTicks = 250;     //20ms per tick (ProcessHeartbeatTask) * 250 = 5s timeout
+};
